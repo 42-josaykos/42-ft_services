c67358b8bb281840139925f4a00864f989cf9941
Jonny Saykosy <contact@josayko.com>
2020-10-14 16:35:48 +0200

    update

--- setup.sh
+++ setup.sh
@@ -10,11 +10,15 @@ echo "Building nginx image..."
 docker build -t services/nginx srcs/nginx > /dev/null
 echo "Building mysql image..."
 docker build -t services/mysql srcs/mysql > /dev/null
+echo "Building wordpress image..."
+docker build -t services/wordpress srcs/wordpress > /dev/null
 
 echo "Applying manifests..."
 kubectl apply -f srcs/manifests/metallb-namespace.yaml > /dev/null
 kubectl apply -f srcs/manifests/metallb.yaml > /dev/null
 kubectl apply -f srcs/manifests/metallb-config.yaml > /dev/null
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 kubectl apply -f srcs/manifests/nginx.yaml > /dev/null
 kubectl apply -f srcs/manifests/mysql.yaml > /dev/null
 
