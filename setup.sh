b0acfbb541f38eb96e7cb995aeb3d8424c1d98fb
Josaykos <josaykos@student.42.fr>
2020-10-27 16:52:03 +0100

    add phpmyadmin

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ echo "Building mysql image..."
 docker build -t services/mysql srcs/mysql > /dev/null
 echo "Building wordpress image..."
 docker build -t services/wordpress srcs/wordpress > /dev/null
+echo "Building phpmyadmin image..."
+docker build -t services/phpmyadmin srcs/phpmyadmin > /dev/null
 
 echo "Applying manifests..."
 kubectl apply -f srcs/manifests/metallb-namespace.yaml > /dev/null
@@ -21,7 +23,8 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 
 kubectl apply -f srcs/manifests/nginx.yaml > /dev/null
 kubectl apply -f srcs/manifests/mysql.yaml > /dev/null
-kubectl apply -f srcs/manifests/wordpress-deploy.yaml > /dev/null
+kubectl apply -f srcs/manifests/wordpress.yaml > /dev/null
+#kubectl apply -f srcs/manifests/phpmyadmin.yaml > /dev/null
 
 minikube dashboard &
 echo "✅ ft_services deployment done"
---
+++ srcs/phpmyadmin/config.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+$cfg['blowfish_secret'] = 'ZK:w0XQHvRXJmTjP;=-}1:XPTSp]d:w0';
+
+/* Servers configuration */
+$i = 1;
+
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['user'] = 'admin';
+$cfg['Servers'][$i]['password'] = 'admin';
+
+/* Directories for saving/loading files from server */
+$cfg['UploadDir'] = '';
+$cfg['PmaAbsoluteUri'] = '192.168.99.203:5000';
+$cfg['SaveDir'] = '';
+
+$cfg['TempDir'] = 'tmp';
