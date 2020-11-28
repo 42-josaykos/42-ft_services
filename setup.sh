0bf697ed0fdb1305a63c7260b9bdabd31f0277d9
Jonny Saykosy <contact@josayko.com>
2020-11-28 18:30:19 +0100

    udpate setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,6 @@ minikube start --vm-driver=docker
 CLUSTER_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 echo "Minikube IP: $CLUSTER_IP"
 
-if [[ $CLUSTER_IP != "192.168.49.2" ]]; then
-  sed -i "s/192.168.49.2/$CLUSTER_IP/g" "srcs/manifests/metallb-config.yaml"
-  sed -i "s/192.168.49.2/$CLUSTER_IP/g" "srcs/ftps/vsftpd.conf"
-  sed -i "s/192.168.49.2/$CLUSTER_IP/g" "srcs/ftps/ssl_test.sh"
-  sed -i "s/192.168.49.2/$CLUSTER_IP/g" "srcs/nginx/nginx.conf"
-  sed -i "s/192.168.49.2/$CLUSTER_IP/g" "srcs/mysql/wordpress.sql"
-fi
-
 eval $(minikube docker-env)
 echo "Building nginx image..."
 docker build -t services/nginx srcs/nginx > /dev/null
